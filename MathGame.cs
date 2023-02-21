d5d6440e14e7f5fd6bf00835f5279c06ea894bcc
wfeliciano20 <william.feliciano2@gmail.com>
2023-02-20 20:12:09 -0500

    Add project files.

---
+++ MathGame.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class MathGame
+    {
+
+        public List<string> RecordOfOperations { get; set; } = new List<string>();
+
+        public int SummationOperation(int a, int b)
+        {
+            RecordOfOperations.Add($"{a} + {b} = {a + b}");
+            return a + b;
+        }
+
+        public int SubstractionOperation(int a, int b)
+        {
+            RecordOfOperations.Add($"{a} - {b} = {a - b}");
+            return a - b;
+        }
+
+        public int MultiplicationOperation(int a, int b)
+        {
+            RecordOfOperations.Add($"{a} * {b} = {a * b}");
+            return a * b;
+        }
+
+        public int DivisionOperation(int a, int b)
+        {
+
+
+            var invalid = true;
+            while (invalid)
+            {
+                if (a < 0 || a > 100)
+                {
+                    Console.WriteLine("Please Enter a valid dividend, valid values are from 0 - 100");
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                else 
+                {
+                    invalid = false;
+                }
+            }
+
+            RecordOfOperations.Add($"{a} / {b} = {a / b}");
+
+            return a / b;
+        }
+
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Welcome to the Math Game");
+            Console.WriteLine("1) Sumation Operation");
+            Console.WriteLine("2) Substraction Operation");
+            Console.WriteLine("3) Multiplication Operation");
+            Console.WriteLine("4) Division Operation");
+            Console.WriteLine("5) Show Past Operations");
+            Console.WriteLine("6) Quit");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using Game;
+
+var mathGame = new MathGame();
+
+var gameOver = false;
+int selection;
+int result;
+int firstNumber = -1;
+int secondNumber = -1;
+
+while (!gameOver)
+{
+    mathGame.DisplayMenu();
+
+    
+    try
+    {
+        selection = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Please Enter a Valid Value: 1 - 6");
+        selection = Convert.ToInt32(Console.ReadLine());
+    }
+
+
+    // Ask for numbers to do operations with
+    if (selection < 5)
+    {
+
+        Console.WriteLine("Please enter the first integer number");
+        while (firstNumber < 0)
+        {
+            try
+            {
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Please Enter a Valid Integer Number:");
+            }
+        }
+
+        Console.WriteLine("Please enter the second integer number");
+
+        while (secondNumber < 0)
+        {
+            try
+            {       
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please Enter a Valid Integer Number:");
+            }
+        }
+    }
+
+    switch (selection)
+    {
+        case 1:
+            result =  mathGame.SummationOperation(firstNumber,secondNumber);
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}\n\n");
+            break;
+        case 2:
+            result = mathGame.SubstractionOperation(firstNumber,secondNumber);
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {result}\n\n");
+            break;
+        case 3:
+            result = mathGame.MultiplicationOperation(firstNumber, secondNumber);
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {result}\n\n");
+            break;
+        case 4:
+            result = mathGame.DivisionOperation(firstNumber, secondNumber);
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {result}\n\n");
+            break;
+        case 5:
+            Console.WriteLine("Previous Operations:");
+            mathGame.RecordOfOperations.ForEach(Console.WriteLine);
+            break;
+        case 6:
+            gameOver = true;
+            break;
+        default:
+            Console.WriteLine("Please enter a valid option: 1 - 6");
+            break;
+    }
+
+    
+}
+
+Console.WriteLine("Thanks for playing this numbers game");
